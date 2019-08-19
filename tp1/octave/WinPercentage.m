0bee331f2443ab9bd00208fac461f05e2d9e8d21
Nicolas <nicoh22@hotmail.com>
2019-08-18 23:30:45 -0300

    Octave scripts

---
+++ tp1/octave/WinPercentage.m
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S octave -qf
+args = argv();
+filename = args{1};
+matches = dlmread(filename, ' ', 1, 1);
+teams = dlmread(filename, ' ', [0 0 0 0]);
+win = zeros(teams, 1);
+played = zeros(teams, 1);
+for i = 1 : size(matches)
+  teamA = matches(i, 1);
+  teamB = matches(i, 3);
+  if matches(i, 2) > matches(i,4)
+    win(teamA) += 1;
+  else %No empate
+    win(teamB) += 1;
+  endif
+  played(teamA) += 1;
+  played(teamB) += 1;
+endfor
+result = win ./ played;
+disp(result);
